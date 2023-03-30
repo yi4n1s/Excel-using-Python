d58cb6a9cfc0292bc1f9ea7df3349826a0db50eb
yi4n1s <114770169+yi4n1s@users.noreply.github.com>
2023-03-30 10:54:51 +0300

    Delete Jaccard Similarity.py

--- Jaccard Similarity.py
+++
@@ -1,7 +0,0 @@
-#The Jaccard Similarity implementation in python using the NetworkX library.
-
-def jaccard_similarity(x,y):
-	
-	intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
-	union_cardinality = len(set.union(*[set(x), set(y)]))
-	return intersection_cardinality/float(union_cardinality)
